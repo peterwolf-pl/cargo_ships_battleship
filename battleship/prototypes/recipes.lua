29aad85abcb341d43cbcb60369d6ac0df4fea14a
Peter Wolf <84736182+peterwolf-pl@users.noreply.github.com>
2026-01-11 04:34:43 +0100

    Add patrol boat with missile turret

--- battleship/prototypes/recipes.lua
+++ battleship/prototypes/recipes.lua
@@ -13,5 +13,20 @@ data:extend{
       {type="item", name="artillery-turret", amount=4}
     },
     results = {{type="item", name="battleship", amount=1}}
+  },
+  {
+    type = "recipe",
+    name = "patrol-boat",
+    enabled = false,
+    energy_required = 8,
+    ingredients = {
+      {type="item", name="steel-plate", amount=80},
+      {type="item", name="engine-unit", amount=25},
+      {type="item", name="iron-gear-wheel", amount=20},
+      {type="item", name="electronic-circuit", amount=15},
+      {type="item", name="explosives", amount=25},
+      {type="item", name="rocket-launcher", amount=1}
+    },
+    results = {{type="item", name="patrol-boat", amount=1}}
   }
 }
